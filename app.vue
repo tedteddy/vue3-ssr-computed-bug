e8f06ec4e17021b46e17e80bf214425ddd4e13cf
kaixuan <kaixuan@xiaohongshu.com>
2021-12-06 16:52:33 +0800

    firsr commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :style="style" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const style: any = computed(() => {
+  if (process.client) {
+    return {
+      height: '200px',
+      width: '200px',
+      background: 'black',
+    }
+  }
+  return {
+    height: '100px',
+    width: '100px',
+    background: 'red',
+  }
+})
+
+</script>
